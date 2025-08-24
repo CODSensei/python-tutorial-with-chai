1acee7d8074061741a9559bb0645b68713204352
CODSensei <krishna2342005@gmail.com>
2025-08-24 17:58:51 +0530

    05_functions/solution2.py completed

---
+++ 05_functions/solution2.py
@@ -0,0 +1,8 @@
+# Problem: Create a function that takes two numbers as parameters and returns their sum.
+def sums(a, b):
+    return a + b
+
+
+a = int(input("Enter a: "))
+b = int(input("Enter b: "))
+print(sums(a, b))
