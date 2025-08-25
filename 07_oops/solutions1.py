1ab8af4fe7bf81e1fce601b710b2f0435ff14157
CODSensei <krishna2342005@gmail.com>
2025-08-25 15:24:50 +0530

    q3

--- 07_oops/solutions1.py
+++ 07_oops/solutions1.py
@@ -1,4 +1,3 @@
-# Problem: Create a Car class with attributes like brand and model. Then create an instance of this class.
 class Car:
     def __init__(self, brand, model):
         self.brand = brand
@@ -8,5 +7,16 @@ class Car:
         return f"{self.brand} {self.model}"
 
 
+class ElectricCar(Car):
+    def __init__(self, brand, model, battery_size):
+        super().__init__(brand, model)
+        self.battery_size = battery_size
+
+    def fullName(self):
+        return super().fullName() + f" {self.battery_size}"
+
+
 my_car = Car("BMW", "M5-CS")
+my_tesla = ElectricCar("Tesla", "Model S", "85kWh")
 print(my_car.fullName())
+print(my_tesla.fullName())
