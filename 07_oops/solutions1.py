aa14a7afb158cb0d8e64c8fafc4f00008a72794a
CODSensei <krishna2342005@gmail.com>
2025-08-25 15:33:43 +0530

    q5

--- 07_oops/solutions1.py
+++ 07_oops/solutions1.py
@@ -9,6 +9,9 @@ class Car:
     def fullName(self):
         return f"{self.__brand} {self.model}"
 
+    def fuel_type(self):
+        return "Petrol or Diesel"
+
 
 class ElectricCar(Car):
     def __init__(self, __brand, model, battery_size):
@@ -18,9 +21,15 @@ class ElectricCar(Car):
     def fullName(self):
         return super().fullName() + f" {self.battery_size}"
 
+    def fuel_type(self):
+        return "Electricity"
+
 
 my_car = Car("BMW", "M5-CS")
 my_tesla = ElectricCar("Tesla", "Model S", "85kWh")
 print(my_car.fullName())
+print(my_car.get_brand())
+print(my_car.fuel_type())
 print(my_tesla.fullName())
 print(my_tesla.get_brand())
+print(my_tesla.fuel_type())
