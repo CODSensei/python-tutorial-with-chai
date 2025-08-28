f6bb070cb66d4ffc0090d4c17ff7fbb0c5fa4bc5
CODSensei <krishna2342005@gmail.com>
2025-08-28 23:39:33 +0530

    chore: boiler plate code

--- project-youtube_video_manager/youtube_manager.py
+++ project-youtube_video_manager/youtube_manager.py
@@ -1,84 +1,65 @@
-import json
+import sqlite3
 
-file_name = "youtube.txt"
+conn = sqlite3.connect("youtube_videos.db")
+cursor = conn.cursor()
 
-
-def load_data():
-    try:
-        with open(file_name, "r") as file:
-            return json.load(file)
-    except FileNotFoundError:
-        return []
-
-
-def save_data_helper(videos):
-    with open(file_name, "w") as file:
-        json.dump(videos, file)
+cursor.execute(
+    """
+    create table if not exits videos (
+        id int primary key,
+        name text not null,
+        time text not null
+    )
+"""
+)
 
 
-def list_all_videos(videos):
-    print()
-    print("*" * 100)
-    for index, video in enumerate(videos, start=1):
-        print(f"{index}. {video['name']}, duration: {video['time']}")
-    print()
-    print("*" * 100)
+def list_videos():
+    pass
 
 
-def add_video(videos):
-    name = input("Enter video name: ")
-    time = input("Enter video time: ")
-    videos.append({"name": name, "time": time})
-    save_data_helper(videos)
+def add_video(name, time):
+    pass
 
 
-def update_video(videos):
-    list_all_videos(videos)
-    index = int(input("Enter the video number to update: "))
-    if 1 <= index <= len(videos):
-        name = input("Enter new video name: ")
-        time = input("Enter new video time: ")
-        videos[index - 1] = {"name": name, "time": time}
-        save_data_helper(videos)
-    else:
-        print("Invalid Index Selected!!")
+def update_video(video_id, name, time):
+    pass
 
 
-def delete_video(videos):
-    list_all_videos(videos)
-    index = int(input("Enter the video number to deleted: "))
-    if 1 <= index <= len(videos):
-        del videos[index - 1]
-        save_data_helper(videos)
-    else:
-        print("Invalid Index Selected!!")
+def delete_video(video_id):
+    pass
 
 
 def main():
-    videos = load_data()
     while True:
-        print("\nYoutube Manager | Choose an Option")
-        print("1. List all youtube videos ")
-        print("2. Add a youtube video ")
-        print("3. Update a youtube video details ")
-        print("4. Delete a youtube video ")
-        print("5. Exit the app ")
+        print("\nYoutube Manager App")
+        print("1. List videos")
+        print("2. Add videos")
+        print("3. Update videos")
+        print("4. Delete videos")
+        print("5. Exit App")
         choice = input("Enter your choice: ")
 
-        match choice:
-            case "1":
-                list_all_videos(videos)
-            case "2":
-                add_video(videos)
-            case "3":
-                update_video(videos)
-            case "4":
-                delete_video(videos)
-            case "5":
-                break
-            case _:
-                print("Invalid Choice!!")
+        if choice == "1":
+            list_videos()
+        elif choice == "2":
+            name = input("Enter the video name: ")
+            time = input("Enter the video time: ")
+            add_video(name, time)
+        elif choice == "3":
+            video_id = input("Enter video id: ")
+            name = input("Enter the video name: ")
+            time = input("Enter the video time: ")
+            update_video(video_id, name, name)
+        elif choice == "4":
+            video_id = input("Enter video id: ")
+            delete_video(video_id)
+        elif choice == "5":
+            break
+        else:
+            print("Invalid Choice!!")
 
+    conn.close()
 
 if __name__ == "__main__":
     main()
